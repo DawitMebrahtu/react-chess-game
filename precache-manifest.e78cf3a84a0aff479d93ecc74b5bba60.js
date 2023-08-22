ea8ea105d2bf350b771c77fddd1f103b4099923a
DawitMebrahtu <dawitmebrahtu93@gmail.com>
2023-08-22 11:50:15 +0300

    Updates

---
+++ precache-manifest.e78cf3a84a0aff479d93ecc74b5bba60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d98494cfba96a87de6c5b293f16c7bd1",
+    "url": "/react-chess-game/index.html"
+  },
+  {
+    "revision": "36ac3dbadc85baa6c67f",
+    "url": "/react-chess-game/static/css/main.7a04927c.chunk.css"
+  },
+  {
+    "revision": "cfff247abd0b01a32060",
+    "url": "/react-chess-game/static/js/2.942a5172.chunk.js"
+  },
+  {
+    "revision": "5b571a43117d7de158db9e221eaa0225",
+    "url": "/react-chess-game/static/js/2.942a5172.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36ac3dbadc85baa6c67f",
+    "url": "/react-chess-game/static/js/main.9bc24b72.chunk.js"
+  },
+  {
+    "revision": "57835196e9d6e778d503",
+    "url": "/react-chess-game/static/js/runtime-main.608c9b4b.js"
+  },
+  {
+    "revision": "3382a95341b72c229183dcc0f1168176",
+    "url": "/react-chess-game/static/media/q_w.3382a953.png"
+  }
+]);
